7ac8104ada5cdb1663233e5696bb3e72978ad8cf
AbdallahMarei <abdullahmari@gmail.com>
2021-10-21 22:30:17 +0300

    added personal folders

---
+++ sourcecode/Fatima/script.js
@@ -0,0 +1,12 @@
+// addEventListener("click", ()=>{
+//     index++
+//     nextQuestion(question[index])
+
+// })
+
+
+// nextQuestion(){
+//     question.answers.forEach(element => {
+        
+//     });
+// }
