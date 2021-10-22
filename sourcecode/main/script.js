27afe31cccb26467ef45f6daf47f8acb57b2d23c
Shymaa-ashqar <shymaa.ashqar@gmail.com>
2021-10-23 00:50:01 +0300

    edited

--- sourcecode/main/script.js
+++ sourcecode/main/script.js
@@ -1,8 +1,7 @@
-
-let password =document.querySelector("#login-pass");
-let confirmPassword = document.getElementById("passwordConfirm")
-let email =document.querySelector(".email-input")
-let isValid = true
+let password = document.querySelector("#login-pass");
+let confirmPassword = document.getElementById("passwordConfirm");
+let email = document.querySelector(".email-input");
+let isValid = true;
 let welcomeHeading = document.getElementById("welcome-user");
 let loginInput_error = document.querySelector("#username_error");
 let email_error = document.querySelector("#email_error");
@@ -13,63 +12,65 @@ let loginForm = document.querySelector(".form-login");
 let loginButton = document.getElementById("login-btn");
 let loginInput = document.querySelector(".login-username");
 
-let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let validEmail =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 let users = [];
 
-function validated(event){
-    event.preventDefault();
-    if (loginInput.value.length < 3){
-        loginInput_error.style.display="block";
-        loginInput.style.border="1px solid red"
-        isValid = false
-    }else{
-        loginInput_error.style.display="none";
-        loginInput.style.border="1px solid silver"
-        isValid = true
-    }
-    
-        if (!(email.value.match(validEmail))){
-        email_error.style.display="block";
-        email.style.border="1px solid red"
-        isValid = false
-    }else{
-        email_error.style.display="none";
-        email.style.border="1px solid silver"
-        isValid = true
-    }
-    if(password.value.length < 6){
-        pass_error.style.display="block";
-        password.style.border="1px solid red"
-        isValid = false
-    }else{
-        pass_error.style.display="none";
-        password.style.border="1px solid silver"
-        isValid = true
-    }
+function validated(event) {
+  event.preventDefault();
+  if (loginInput.value.length < 3) {
+    loginInput_error.style.display = "block";
+    loginInput.style.border = "1px solid red";
+    isValid = false;
+  } else {
+    loginInput_error.style.display = "none";
+    loginInput.style.border = "1px solid silver";
+    isValid = true;
+  }
 
-    if(confirmPassword.value !== password.value){
-      
-        confirm_error.style.display="block";
-        confirmPassword.style.border="1px solid red"
-        isValid = false
-    }else{
-        confirm_error.style.display="none";
-        confirmPassword.style.border="1px solid silver"
-    }
-    if (isValid == true){
-        localStorage.setItem(`username ${loginInput.value}`, loginInput.value);
-        window.location.href = "quiz.html";
-    } 
+  if (!email.value.match(validEmail)) {
+    email_error.style.display = "block";
+    email.style.border = "1px solid red";
+    isValid = false;
+  } else {
+    email_error.style.display = "none";
+    email.style.border = "1px solid silver";
+    isValid = true;
+  }
+  if (password.value.length < 6) {
+    pass_error.style.display = "block";
+    password.style.border = "1px solid red";
+    isValid = false;
+  } else {
+    pass_error.style.display = "none";
+    password.style.border = "1px solid silver";
+    isValid = true;
+  }
+
+  if (confirmPassword.value !== password.value) {
+    confirm_error.style.display = "block";
+    confirmPassword.style.border = "1px solid red";
+    isValid = false;
+  } else {
+    confirm_error.style.display = "none";
+    confirmPassword.style.border = "1px solid silver";
+  }
+  if (isValid == true) {
+    localStorage.setItem(`username ${loginInput.value}`, loginInput.value);
+    window.location.href = "quiz.html";
+  }
 }
 
- loginForm.addEventListener("submit", function(e) {
-    e.preventDefault();
-    if(loginInput.value === localStorage.getItem(`username ${loginInput.value}`)){
-        window.location.href = "quiz.html";
-    }else{
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (
+    loginInput.value === localStorage.getItem(`username ${loginInput.value}`)
+  ) {
+    window.location.href = "quiz.html";
+  } else {
     alert("this username is not found, please register");
-    }  
-})
-
-
+  }
+});
+document.getElementById("welcome-user").innerHTML =
+  "welcome" + localStorage.getItem(`username ${loginInput.value}`);
---
+++ sourcecode/main/scriptquiz.js
@@ -0,0 +1,2 @@
+var answer = localStorage.key(1);
+document.getElementById("welcome-user").innerHTML = "welcome" + answer;
